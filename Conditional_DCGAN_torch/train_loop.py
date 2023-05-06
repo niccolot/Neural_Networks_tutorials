4c17c7eeed9493de8427ff4412c349f2ecb5b83d
niccolot <nico_tiezzi98@hotmail.it>
2023-05-06 19:10:30 +0200

    added comments

--- Conditional_DCGAN_torch/train_loop.py
+++ Conditional_DCGAN_torch/train_loop.py
@@ -10,7 +10,9 @@ def train_step(discriminator,
                labels, 
                device, 
                latent_space_dim):
-    
+    '''
+    train step for GAN training with k=1 steps for the discriminator
+    '''
     images.to(device)
     labels.to(device)
     labels = labels.unsqueeze(1)  # (batch) -> (batch,1) in order to be fed to the model
@@ -26,6 +28,18 @@ def train_step(discriminator,
 
     noise_vector = torch.randn(images.size(0), latent_space_dim, device=device)
     fake_images = generator((noise_vector, labels))
+
+    '''
+    the .detach() for the generated imaged is done because when .backward() is called on the
+    discriminator loss it also cancels the computational graph, resulting in the impossibility
+    of doing backpropagation on the generator with the loss calculated with the same generated images
+    (because the tensor belonging to those images is gone with the computational graph of the discriminator).
+
+    .detach() then preserves the fake images for future backpropagation, it also saves time because
+    when .backward() is called on the discriminator's loss the gradients w.r.t. the fake images are 
+    not calculated but this is not the main reason why one has to detach the fake images' tensors from
+    the computational graph 
+    '''
     discriminator_fake_outputs = discriminator((fake_images.detach(), labels))
     d_fake_loss = d_loss(discriminator_fake_outputs,fake_targets)
 
